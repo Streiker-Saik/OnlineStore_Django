15190b3d9decc18cbc80a7781aec7567eb2fed0e
Alexei <streiker.saik@gmail.com>
2025-06-04 10:56:12 +0300

    add models: Category, Product

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Модель представляющая категорию
+
+    Атрибуты:
+        name(str): Название категории
+        description(str): Описание категории
+    """
+
+    name: str = models.CharField(max_length=150, verbose_name="Наименование")
+    description: str = models.CharField(max_length=250, verbose_name="Описание")
+
+    def __str__(self) -> str:
+        """
+        Строковое представление продуктов
+        :return: Наименование категории
+        """
+        return self.name
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+        ordering = ["name"]
+
+
+class Product(models.Model):
+    """
+    Модель, представляющая продукт
+    Атрибуты:
+        name(str): Наименование продукта
+        description(str): Описание продукта
+        image(ImageField): Изображение продукта
+        category(ForeignKey): Связь с категорией, к которой принадлежит продукт
+        price(float): Цена продукта
+        created_at(datetime): Дата и время создания продукта
+        updated_at(datetime): Дата и время последнего изменения продукта
+    """
+
+    name: str = models.CharField(max_length=150, verbose_name="Наименование")
+    description: str = models.CharField(max_length=150, verbose_name="Описание")
+    image = models.ImageField(upload_to='images/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
+    price: float = models.FloatField(verbose_name="Цена")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+
+    def __str__(self) -> str:
+        """
+        Строковое представление продуктов
+        :return: Наименование категории
+        """
+        return self.name
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
+        ordering = ["name"]
