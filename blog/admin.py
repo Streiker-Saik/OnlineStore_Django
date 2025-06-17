1392b39c071d062e20d7b2d9842efe96e89e150c
Alexei <streiker.saik@gmail.com>
2025-06-17 14:48:08 +0300

    add admin: blogpost

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BlogPost
+
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    """Класс для работы администратора с категориями"""
+
+    list_display = ("id", "title", "content", "publication", "views_count")
+    list_filter = (
+        "publication",
+        "views_count",
+    )
+    search_fields = (
+        "title",
+        "content",
+    )
+    ordering = ("created_at",)
