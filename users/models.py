c2ed5c9aab2f6e8381552f99327c113bcc6b0e09
Alexei <streiker.saik@gmail.com>
2025-06-26 16:27:40 +0300

    add models: CustomUser

--- users/models.py
+++ users/models.py
@@ -3,5 +3,26 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    """"""
-    pass
+    """
+    Представление кастомного пользователя, расширяющее AbstractUser.
+    Поле авторизации с username изменено на email.
+    Так же username обязательное поле при авторизации
+    Атрибуты:
+        avatar(ImageField): Аватар (изображение)
+        phone_number(str): Номер телефона
+        country(str): Страна
+    """
+
+    avatar = models.ImageField("avatars/", blank=True, null=True, verbose_name="Аватар")
+    phone_number = models.CharField(max_length=15, blank=True, null=True, verbose_name="Номер телефона")
+    country = models.CharField(max_length=65, blank=True, null=True, verbose_name="Страна")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        """
+        Строковое представление класса.
+        :return: Электронная почта
+        """
+        return self.email
