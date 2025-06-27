8c98746a4ce537e44a9dae17b8746475a7bca820
Alexei <streiker.saik@gmail.com>
2025-06-27 09:01:38 +0300

    corrected CustomUser and add migrations

--- users/models.py
+++ users/models.py
@@ -8,12 +8,14 @@ class CustomUser(AbstractUser):
     Поле авторизации с username изменено на email.
     Так же username обязательное поле при авторизации
     Атрибуты:
+        email(str): Уникальный email - используется для авторизации
         avatar(ImageField): Аватар (изображение)
         phone_number(str): Номер телефона
         country(str): Страна
     """
 
-    avatar = models.ImageField("avatars/", blank=True, null=True, verbose_name="Аватар")
+    email = models.EmailField(unique=True, verbose_name="Электронная почта")
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True, verbose_name="Аватар")
     phone_number = models.CharField(max_length=15, blank=True, null=True, verbose_name="Номер телефона")
     country = models.CharField(max_length=65, blank=True, null=True, verbose_name="Страна")
 
