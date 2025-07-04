7cbb5994eac1501951c25781f2274e9ebbd8bea0
Alexei <streiker.saik@gmail.com>
2025-07-04 11:59:47 +0300

    add redis and correction fixture + README.md

--- config/settings.py
+++ config/settings.py
@@ -151,3 +151,10 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 LOGIN_URL = "users:login"
 LOGIN_REDIRECT_URL = "catalog:home"
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+    }
+}
